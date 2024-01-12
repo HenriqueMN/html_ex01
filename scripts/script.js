43341b6ab8f687bc2923f8a63a7ae7b82cf1ec13
Henrique M. N. <henriquemancininevado@gmail.com>
2024-01-12 15:54:00 -0300

    Implementação de dois botões com JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let gato_malhado = document.querySelector('#gato_malhado');
+
+gato_malhado.style.visibility = 'hidden';
+
+function mostrarImagem(){
+    gato_malhado.style.visibility = 'visible';
+}
+
+function meow(){
+    alert('meow!')
+}
